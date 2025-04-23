0f7a2c2275a4b9ab70c8a4d21ab8106a43e9053f
SebastianNeguey <sebastianleonardo98@gmail.com>
2025-04-23 18:21:48 -0400

    Beta Portafolio

---
+++ src/data/proyectos.js
@@ -0,0 +1,29 @@
+// src/data/proyectos.js
+export default [
+    {
+      titulo: "Proyecto 1",
+      descripcion: "Este es el proyecto número 1.",
+      imagenUrl: "/images/ejemplo.jpg",  // Asegúrate de tener las imágenes en la carpeta /public/images
+      link: "https://github.com/SebastianNeguey"
+    },
+    {
+      titulo: "Proyecto 2",
+      descripcion: "Este es el proyecto número 2.",
+      imagenUrl: "/images/ejemplo.jpg",  // Asegúrate de tener las imágenes en la carpeta /public/images
+      link: "https://github.com/miusuario/proyecto2"
+    },
+    {
+      titulo: "Proyecto 3",
+      descripcion: "Este es el proyecto número 3.",
+      imagenUrl: "/images/ejemplo.jpg",  // Asegúrate de tener las imágenes en la carpeta /public/images
+      link: "https://github.com/miusuario/proyecto2"
+    },
+    {
+      titulo: "Proyecto 4",
+      descripcion: "Este es el proyecto número 4.",
+      imagenUrl: "/images/ejemplo.jpg",  // Asegúrate de tener las imágenes en la carpeta /public/images
+      link: "https://github.com/miusuario/proyecto2"
+    },
+    // Añadir más proyectos aquí
+  ];
+  
