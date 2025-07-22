83dcba1304145edbbb87a179bfc243c023587f52
SebastianNeguey <sebastianleonardo98@gmail.com>
2025-07-22 17:28:00 -0400

    Actualizacion astro.congif

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: 'https://SebastianNeguey.github.io',
+    site: 'https://SebastianNeguey.github.io/portafolio-astro',
     base: '/portafolio-astro/',
 });
