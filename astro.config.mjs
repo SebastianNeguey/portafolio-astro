c4076716197e815533cb30fdb9273d92b959b2f2
SebastianNeguey <sebastianleonardo98@gmail.com>
2025-07-23 00:23:24 -0400

    Actualizacion ProyectoCard

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/portafolio-astro/',
     site: 'https://SebastianNeguey.github.io/portafolio-astro/',
     output: 'static'
 });
